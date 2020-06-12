078cadb208c9d58d7ffe4934fbd519b967748c87
cjx <13269169250@163.com>
2020-06-12 09:38:56 +0800

    ---------------

--- app/src/main/java/com/example/myapplication/bean/User.kt
+++ app/src/main/java/com/example/myapplication/bean/User.kt
@@ -3,6 +3,9 @@ package com.example.myapplication.bean
 import java.io.Serializable
 
 class User:Serializable{
+    constructor(){
+        this.javaClass
+    }
     var age=0;
     var name="";
     var address="";
