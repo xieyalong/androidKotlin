5c7013afba23ec9d127c755c1481686049477d13
cjx <13269169250@163.com>
2020-06-15 10:21:33 +0800

    -----

--- app/src/main/java/com/example/myapplication/activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication.activity
 
+import android.Manifest
 import android.text.TextUtils
 import android.view.View
 import com.example.myapplication.R
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.runtime.Permission
 import kotlinx.android.synthetic.main.activity_login.*
 import lib.common.activity.BaseActivity
 import lib.common.util.UtilToast
@@ -14,6 +17,7 @@ class  LoginActivity: BaseActivity() {
 
     override fun initViews() {
         tv_login.setOnClickListener(this)
+        permission()
     }
 
     override fun onClick(v: View?) {
@@ -35,5 +39,49 @@ class  LoginActivity: BaseActivity() {
             return
         }
         MainActivity.startActivity(this);
+    }
+    fun permission() {
+//        AndPermission.with(this).permission(
+//            Manifest.permission.INTERNET,
+//            Manifest.permission.ACCESS_NETWORK_STATE,
+//            Manifest.permission.ACCESS_WIFI_STATE,
+//            Manifest.permission.CHANGE_WIFI_STATE,
+//            Manifest.permission.WRITE_SETTINGS,
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//            Manifest.permission.RECORD_AUDIO,
+//            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+//            Manifest.permission.SYSTEM_ALERT_WINDOW,
+//            Manifest.permission.VIBRATE,
+//            Manifest.permission.READ_PHONE_STATE,
+//            Manifest.permission.BLUETOOTH,
+//            Manifest.permission.CAMERA,
+//            Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+//            Manifest.permission.CALL_PHONE
+//        ).requestCode(0)
+//            .callback(object : PermissionListener {
+//            override fun onSucceed(
+//                requestCode: Int,
+//                grantPermissions: List<String>
+//            ) {
+//                println(">]权限回调成功")
+//            }
+//
+//            override fun onFailed(
+//                requestCode: Int,
+//                deniedPermissions: List<String>
+//            ) {
+//                println(">]权限回调失败")
+//            }
+//        }).start()
+
+
+        AndPermission.with(this)
+            .runtime()
+            .permission(Permission.Group.STORAGE).permission(Permission.CAMERA)
+            .onGranted({ permissions ->
+            })
+            .onDenied({ permissions -> })
+            .start()
     }
 }
